5328bc8d48888b45b9154f73a408c735b4ef2670
kostadinM29 <68953020+kostadinM29@users.noreply.github.com>
2021-06-04 15:35:33 +0300

    Added MVC lecture and workshop
    Added MVC lecture and workshop

---
+++ C# Web Basics - MVC Introduction/CSharp-Web-Server-main/MyWebServer/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWebServer
+{
+    using System.Threading.Tasks;
+    using MyWebServer.Server;
+    using MyWebServer.Server.Responses;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await new HttpServer(routes => routes
+                .MapGet("/", new TextResponse("Hello from Ivo!"))
+                .MapGet("/Cats", new TextResponse("<h1>Hello from the cats!</h1>", "text/html"))
+                .MapGet("/Dogs", new TextResponse("<h1>Hello from the dogs!</h1>", "text/html")))
+            .Start();
+    }
+}
